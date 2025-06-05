7de4ad6aa62dd7095ea21635f120fe8dc29484fc
Andres <elmarine18@gmail.com>
2025-06-05 10:36:55 +0200

    update: assets & Library

---
+++ Assets/Scripts/Interfaces/CardData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CardData
+{
+    public string cardName;
+    public int cost;
+    public string cardType;
+    public string description;
+    public int attack;
+    public int defense;
+    public Sprite art;
+    public Sprite background;
+}
